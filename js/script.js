00210c963df3c16970fe053aec7060d0eaeb73ba
max_g <maxgongob@gmail.com>
2025-04-07 16:39:07 -1000

    Completed challenge

--- js/script.js
+++ js/script.js
@@ -28,3 +28,27 @@ const clover = createPet("Clover", "rabbit");
 const baxter = createPet("Baxter", "hamster");
 const cleo = createPet("Cleo", "dog");
 const francine = createPet("Francine", "turtle");
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+  for (let item of petArray) {
+    let status = "ready to play!";
+    if (item.isTired >= 7) {
+      status = "sleeping"
+    };
+    let li = document.createElement("li");
+    li.innerHTML = `<span class="pet-name">${item.name}</span> the ${item.species} is ${status}.`;
+    pets.append(li);
+  };
+};
+
+statusButton.addEventListener("click", function (){
+  showPets(allPets);
+});
+
+
